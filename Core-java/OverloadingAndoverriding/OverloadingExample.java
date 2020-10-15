a95862c8d1582cff816bc1108165beb257bac83d
Harish Ingole <67138369+harishingole@users.noreply.github.com>
2020-10-15 12:27:34 +0530

    Operator Overloading

---
+++ Core-java/OverloadingAndoverriding/OverloadingExample.java
@@ -0,0 +1,9 @@
+class Test 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hello"+"World!");
+		System.out.println(10+20);
+		System.out.println(10+20+"\nHello"+"World!\n"+10+20);
+	}
+}
