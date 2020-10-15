c38e217405927b662d872b5aac71b965f84dc3c0
Harish Ingole <67138369+harishingole@users.noreply.github.com>
2020-10-15 12:32:46 +0530

    Create Instance_Block1.java

---
+++ Core-java/Instance-Static Block/Instance_Block1.java
@@ -0,0 +1,14 @@
+class Test 
+{
+	Test()
+	{
+		System.out.println("0-Args");
+	}
+	{
+		System.out.println("Instance Blocks");
+	}
+	public static void main(String[] args) 
+	{
+		new Test();
+	}
+}
