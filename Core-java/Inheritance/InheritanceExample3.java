dcf7828542a8709887160aa8cbede8862fc09b32
Harish Ingole <67138369+harishingole@users.noreply.github.com>
2020-10-15 12:11:23 +0530

    Final class not allowed to inherit

---
+++ Core-java/Inheritance/InheritanceExample3.java
@@ -0,0 +1,10 @@
+// prevention
+final class A
+{
+}
+class B extends A 
+{
+}
+/*E:\Java Program>javac inheritance3.java
+inheritance3.java:5: error: cannot inherit from final A
+class B extends A*/
