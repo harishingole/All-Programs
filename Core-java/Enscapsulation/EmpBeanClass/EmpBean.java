29cc01bd1e8b30ae5249c84570b4adf0e79fe94b
Harish Ingole <67138369+harishingole@users.noreply.github.com>
2020-10-15 12:03:12 +0530

    Achieve encapsulation using java bean class

---
+++ Core-java/Enscapsulation/EmpBeanClass/EmpBean.java
@@ -0,0 +1,23 @@
+class EmpBean{
+
+	private int eid;
+	private String ename;
+	
+	public void setEid(int eid)
+	{
+		this.eid = eid;
+	}
+	public void setEname(String ename)
+	{
+		this.ename=ename;
+	}
+	
+	public int getEid()
+	{
+		return eid;
+	}
+	public String getEname()
+	{
+		return ename;
+	}
+}
